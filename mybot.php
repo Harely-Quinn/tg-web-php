33d27f87f6806d73d11d998b1755e542c5f91c90
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-20 21:25:19 +0300

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -29,7 +29,7 @@ $msgType = $telegram->getUpdateType();
 
 
 if(strtolower($text) == 'test'){
-    $web_app = (object)['url' => "https://harely-quinn.github.io/web-test/webappcontent.telegram.org/demo.html"];
+    $web_app = (object)['url' => "https://harely-tg-web.herokuapp.com/webAppTest.html"];
 
 	$option = array( 
 		array(
